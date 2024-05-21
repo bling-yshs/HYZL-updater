8bb3b8c3c3d5fa768493933508624a4b10c87930
bling-yshs <377178599@qq.com>
2024-05-21 17:06:41 +0800

    release

---
+++ .github/scripts/release.cjs
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// 读取updater.json文件
+let buffer = fs.readFileSync('./updater.json');
+let obj = JSON.parse(buffer.toString());
+
+// 获取程序输入，两个参数，一个version，changelog，都是字符串
+let version = process.argv[2]
+let changelog = process.argv[3]
+
+// 获取当前秒级时间戳
+let timestamp = Math.floor(new Date().getTime() / 1000);
+
+obj.unshift({
+  version,
+  "url": `https://mirror.ghproxy.com/https://github.com/bling-yshs/YzLauncher-windows/releases/download/${version}/YzLauncher-windows.exe`,
+  timestamp,
+  changelog,
+  "deprecated": false
+})
+
+// 写入updater.json文件
+fs.writeFileSync('./updater.json', JSON.stringify(obj, null, 2));
