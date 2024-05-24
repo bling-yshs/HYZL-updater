78965c5ed233d3206cc5fbbe01418c8c427b2440
bling-yshs <377178599@qq.com>
2024-05-24 13:34:26 +0800

    修复

--- .github/scripts/release.cjs
+++ .github/scripts/release.cjs
@@ -1,4 +1,5 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = require('fs').promises;
 const crypto = require('crypto');
 const https = require('https');
 
@@ -11,14 +12,14 @@ async function downloadFile(url, dest) {
         file.close(resolve);
       });
     }).on('error', err => {
-      fs.unlink(dest);
+      fsp.unlink(dest);
       reject(err);
     });
   });
 }
 
 async function calculateMD5(filePath) {
-  const buffer = await fs.readFile(filePath);
+  const buffer = await fsp.readFile(filePath);
   const hash = crypto.createHash('md5');
   hash.update(buffer);
   return hash.digest('hex');
@@ -26,7 +27,7 @@ async function calculateMD5(filePath) {
 
 async function main() {
   // 读取updater.json文件
-  let buffer = await fs.readFile('./updater.json');
+  let buffer = await fsp.readFile('./updater.json');
   let obj = JSON.parse(buffer.toString());
   
   // 获取程序输入，两个参数，一个version，changelog，都是字符串
@@ -44,7 +45,7 @@ async function main() {
   await downloadFile(url, dest);
   let md5 = await calculateMD5(dest);
   // 删除掉刚刚下载的文件
-  await fs.unlink(dest);
+  await fsp.unlink(dest);
   obj.unshift({
     version,
     url,
@@ -55,7 +56,7 @@ async function main() {
   });
   
   // 写入updater.json文件
-  await fs.writeFile('./updater.json', JSON.stringify(obj, null, 2));
+  await fsp.writeFile('./updater.json', JSON.stringify(obj, null, 2));
 }
 
 main().catch(console.error);
