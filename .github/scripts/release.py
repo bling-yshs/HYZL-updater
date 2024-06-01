c0c1e5ae3eca8481ef2657a2b8382605ba5b3ac6
bling-yshs <377178599@qq.com>
2024-06-01 13:04:02 +0800

    修复

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -5,6 +5,7 @@ import time
 import json
 from pathlib import Path
 
+
 def download_file(url, dest):
     dirname = os.path.dirname(dest)
     if not os.path.exists(dirname):
@@ -15,6 +16,7 @@ def download_file(url, dest):
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
 
+
 def calculate_md5(file_path):
     hash_md5 = hashlib.md5()
     with open(file_path, 'rb') as f:
@@ -22,9 +24,10 @@ def calculate_md5(file_path):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def main():
     # 读取updater.json文件
-    with open('./updater.json', 'r') as f:
+    with open('./updater.json', 'r', encoding='utf-8') as f:
         obj = json.load(f)
 
     # 获取程序输入，两个参数，一个version，changelog，都是字符串
@@ -43,9 +46,10 @@ def main():
 
     while True:
         response = requests.head(file_url)
-        if response.status_code == 200:
+        if response.status_code == 302:
             break
         else:
+            print("没找到文件，等待10秒后重试")
             time.sleep(10)
 
     # 下载上面链接的文件，并计算它的md5
@@ -63,8 +67,9 @@ def main():
     })
 
     # 写入updater.json文件
-    with open('./updater.json', 'w') as f:
-        json.dump(obj, f, indent=2)
+    with open('./updater.json', 'w', encoding='utf-8') as f:
+        json.dump(obj, f, ensure_ascii=False, indent=2)
+
 
 if __name__ == "__main__":
     main()
